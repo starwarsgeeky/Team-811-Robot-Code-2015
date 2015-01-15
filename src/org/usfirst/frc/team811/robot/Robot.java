52e0aaa81f658e5013fdfc8a9201ea3a414bb694
grainnecasey <g_r_casey@live.com>
2015-01-14 20:27:13 -0500

    Initializing Solenoid

--- src/org/usfirst/frc/team811/robot/Robot.java
+++ src/org/usfirst/frc/team811/robot/Robot.java
@@ -15,8 +15,11 @@ public class Robot extends IterativeRobot {
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
+	
+	new DoubleSolenoid thing; 
+	
     public void robotInit() {
-
+    	thing = new DoubleSolenoid(1);
     }
 
     /**
