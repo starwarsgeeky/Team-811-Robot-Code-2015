d8205788323dbaa54bb47b4f27e04aac4482dad2
grainnecasey <g_r_casey@live.com>
2015-01-28 21:10:23 -0500

    adding final robot code organization stuff using command based robot

---
+++ src/org/usfirst/frc811/RobotCode2015/subsystems/ToteTrigger.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc811.RobotCode2015.subsystems;
+
+import edu.wpi.first.wpilibj.buttons.Trigger;
+
+/**
+ *
+ */
+public class ToteTrigger extends Trigger {
+    
+    public boolean get() {
+        return false;
+    }
+}
