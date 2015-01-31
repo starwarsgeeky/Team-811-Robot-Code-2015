b84d05b5c13093d627c95b3e6cda38a52408b943
grainnecasey <g_r_casey@live.com>
2015-01-30 20:13:03 -0500

    claw commands (bc Erica's commit didn't work)

--- src/org/usfirst/frc811/RobotCode2015/commands/close_claw.java
+++ src/org/usfirst/frc811/RobotCode2015/commands/close_claw.java
@@ -34,6 +34,7 @@ public class  close_claw extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.claw.Close();
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- src/org/usfirst/frc811/RobotCode2015/commands/open_claw.java
+++ src/org/usfirst/frc811/RobotCode2015/commands/open_claw.java
@@ -34,6 +34,7 @@ public class  open_claw extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.claw.Open();
     }
 
     // Make this return true when this Command no longer needs to run execute()
