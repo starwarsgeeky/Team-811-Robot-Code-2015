7b6c630f427cc98fd852745d67811af1c2142d50
grainnecasey <g_r_casey@live.com>
2015-02-03 20:07:35 -0500

    adding lift with joy function

--- src/org/usfirst/frc811/RobotCode2015/commands/moving_lift_w_joysticks.java
+++ src/org/usfirst/frc811/RobotCode2015/commands/moving_lift_w_joysticks.java
@@ -22,6 +22,7 @@ public class moving_lift_w_joysticks extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.lift.Lift_w_joy();
     }
 
     // Make this return true when this Command no longer needs to run execute()
