c27eb2d39142dc0418ed88c108047aa3f7c8bb97
Pazu77 <lightnesshaku@gmail.com>
2015-02-03 20:22:27 -0500

    Doing grab box command

--- src/org/usfirst/frc811/RobotCode2015/commands/grab_box.java
+++ src/org/usfirst/frc811/RobotCode2015/commands/grab_box.java
@@ -34,5 +34,7 @@ public class grab_box extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
+    	addSequential(new lift_down());
+    	addSequential(new lift_up());
     }
 }
