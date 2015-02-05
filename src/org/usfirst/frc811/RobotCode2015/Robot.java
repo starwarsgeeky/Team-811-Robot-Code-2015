2c1cf50d8f6df7d7202f484c5a85b3729643d7ce
grainnecasey <g_r_casey@live.com>
2015-02-04 20:54:17 -0500

    changed OI

--- src/org/usfirst/frc811/RobotCode2015/Robot.java
+++ src/org/usfirst/frc811/RobotCode2015/Robot.java
@@ -75,7 +75,8 @@ public class Robot extends IterativeRobot {
 
     public void autonomousInit() {
         // schedule the autonomous command (example)
-        if (autonomousCommand != null) autonomousCommand.start();
+        //if (autonomousCommand != null) autonomousCommand.start();
+        
     }
 
     /**
