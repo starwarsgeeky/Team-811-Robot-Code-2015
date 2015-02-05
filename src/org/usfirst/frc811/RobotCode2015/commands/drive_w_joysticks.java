c9fce2ab51aadcdd5ed6a49b235b50718e1e96ab
grainnecasey <g_r_casey@live.com>
2015-02-04 19:16:18 -0500

    adding is finished methods and such

--- src/org/usfirst/frc811/RobotCode2015/commands/drive_w_joysticks.java
+++ src/org/usfirst/frc811/RobotCode2015/commands/drive_w_joysticks.java
@@ -34,6 +34,7 @@ public class  drive_w_joysticks extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.drive.driveWithJoy();
     }
 
     // Make this return true when this Command no longer needs to run execute()
