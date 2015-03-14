51005152321eb15b052b8c6f2298637ac61f75ee
Pazu77 <lightnesshaku@gmail.com>
2015-03-14 12:26:32 -0400

    ThreeToteAutov2

--- src/org/usfirst/frc811/RobotCode2015/Robot.java
+++ src/org/usfirst/frc811/RobotCode2015/Robot.java
@@ -79,6 +79,7 @@ public class Robot extends IterativeRobot implements Config {
         
     }
 
+    
     /**
      * This function is called when the disabled button is hit.
      * You can use it to reset subsystems before shutting down.
